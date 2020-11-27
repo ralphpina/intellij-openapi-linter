3044858d1ad01230e91cd85fa381731cfc177b18
GitHub Action <action@github.com>
2020-11-27 17:34:24 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/ralphpina/intellijopenapilinter/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.ralphpina.intellijopenapilinter.services
+
+import com.github.ralphpina.intellijopenapilinter.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
