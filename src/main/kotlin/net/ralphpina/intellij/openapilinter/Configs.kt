a519ba69fbd6844d15f69c7d09350af18dbd499d
Ralph Pina <ralph.pina@affirm.com>
2020-11-30 11:41:48 -0800

    Update fetch ruleset method

--- src/main/kotlin/net/ralphpina/intellij/openapilinter/Configs.kt
+++ src/main/kotlin/net/ralphpina/intellij/openapilinter/Configs.kt
@@ -1,12 +1,11 @@
 package net.ralphpina.intellij.openapilinter
 
 import com.intellij.ide.util.PropertiesComponent
-import com.intellij.openapi.project.Project
 
 private const val PROP_RULESET_KEY = "net.ralphpina.intellij.openapilinter.ruleSetPath"
 
-internal fun fetchRuleSetPath(project: Project) =
-        PropertiesComponent.getInstance(project).getValue(PROP_RULESET_KEY)
+fun fetchRuleSetPath() =
+        PropertiesComponent.getInstance().getValue(PROP_RULESET_KEY)
 
 fun updateRuleSetPath(path: String) =
         PropertiesComponent.getInstance().setValue(PROP_RULESET_KEY, path)
